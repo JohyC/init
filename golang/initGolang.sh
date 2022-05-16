72b89b864323f86892430d32475730d2a5b8be02
Johy <c13340908272@outlook.com>
2022-05-16 20:45:49 +0800

    feat: update initGolang;

--- golang/initGolang.sh
+++ golang/initGolang.sh
@@ -12,13 +12,13 @@ read -r -t 30 -p "write golang packages url;if not set then use 1.18.2 >" url
 if [ ! -n "$url" ]; then
   url='https://go.dev/dl/go1.18.2.linux-amd64.tar.gz'
 fi
-# wget $url -O golang.tar.gz
-# if [ ! "$?" == "0" ]; then
-#   echo 'wget error!' 1>&2
-#   exit 1
-# fi
-# tar -xvf golang.tar.gz -C $custom
-# ln -s $custom/go/bin/* /usr/local/bin
+wget $url -O golang.tar.gz
+if [ ! "$?" == "0" ]; then
+  echo 'wget error!' 1>&2
+  exit 1
+fi
+tar -xvf golang.tar.gz -C $custom
+ln -s $custom/go/bin/* /usr/local/bin
 if [ -n "$ZDOTDIR" ]; then
   echo -e 'export GOENV=/usr/local/custom/go/.config/env
 export PATH=$PATH:/usr/local/custom/gowork/bin' >>$ZDOTDIR/.custom/.zprofile
